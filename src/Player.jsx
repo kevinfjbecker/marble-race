685a3cc45b811c4f6cf9986761b0ce4fc979e618
Kevin Becker <kevinfjbecker@gmail.com>
2024-02-24 10:25:54 +0100

    Ball

--- src/Player.jsx
+++ src/Player.jsx
@@ -1,5 +1,9 @@
 export default function Player()
 {
     return <>
+        <mesh castShadow>
+            <icosahedronGeometry args={ [ 0.3, 1 ] } />
+            <meshStandardMaterial flatShading color="mediumpurple" />
+        </mesh>
     </>
 }
