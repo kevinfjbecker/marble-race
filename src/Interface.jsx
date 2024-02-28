abd8595ffb683fafa7572b0674cad0cbb2e67923
Kevin Becker <kevinfjbecker@gmail.com>
2024-02-28 21:16:18 +0100

    Keyboard inputs

--- src/Interface.jsx
+++ src/Interface.jsx
@@ -1,5 +1,13 @@
+import { useKeyboardControls } from "@react-three/drei"
+
 export default function Interface()
 {
+    const forward = useKeyboardControls((state) => state.forward)
+    const backward = useKeyboardControls((state) => state.backward)
+    const leftward = useKeyboardControls((state) => state.leftward)
+    const rightward = useKeyboardControls((state) => state.rightward)
+    const jump = useKeyboardControls((state) => state.jump)
+
     return <div className="interface">
 
         {/* Time */}
@@ -8,5 +16,20 @@ export default function Interface()
         {/* Restart */}
         <div className="restart">Restart</div>
         
+        {/* Controls */}
+        <div className="controls">
+            <div className="raw">
+                <div className={ `key ${ forward ? 'active' : '' }` }></div>
+            </div>
+            <div className="raw">
+                <div className={ `key ${ leftward ? 'active' : '' }` }></div>
+                <div className={ `key ${ backward ? 'active' : '' }` }></div>
+                <div className={ `key ${ rightward ? 'active' : '' }` }></div>
+            </div>
+            <div className="raw">
+                <div className={`key large ${ jump ? 'active' : '' }`}></div>
+            </div>
+        </div>
+
     </div>
 }
